544733d3f39904ae51e113c825b226f31169027a
leapwill <leapwill@users.noreply.github.com>
2022-08-28 18:07:26 -0500

    Slightly de-prioritize CPU and send to backgroup to enable concurrent event handling

--- src/watch.sh
+++ src/watch.sh
@@ -4,5 +4,5 @@ cp -n /src/autoconv.json /config/
 
 /usr/bin/inotifywait --monitor --recursive --quiet --csv -e close_write -e moved_to /watch |
 while read -r notif; do
-    pwsh -File /src/Start-Autoconv.ps1 "$notif" -ErrorAction Stop
+    nice -n 1 pwsh -File /src/Start-Autoconv.ps1 "$notif" -ErrorAction Stop &
 done

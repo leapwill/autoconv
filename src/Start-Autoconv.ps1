21eaa099b46a551331fd0e47fc57d00fe466155d
leapwill <leapwill@users.noreply.github.com>
2022-09-10 15:14:08 -0500

    Skip non-video files

--- src/Start-Autoconv.ps1
+++ src/Start-Autoconv.ps1
@@ -27,7 +27,8 @@ $FileName = $WatchedDir + $FileName
 $File = Get-Item -LiteralPath $FileName
 [string[]]$VIDEO_EXTENSIONS = @('.3g2','.3gp','.3gp2','.3gpp','.amr','.amv','.asf','.avi','.bdmv','.bik','.d2v','.divx','.drc','.dsa','.dsm','.dss','.dsv','.evo','.f4v','.flc','.fli','.flic','.flv','.hdmov','.ifo','.ivf','.m1v','.m2p','.m2t','.m2ts','.m2v','.m4b','.m4p','.m4v','.mkv','.mk3d','.mp2v','.mp4','.mp4v','.mpe','.mpeg','.mpg','.mpls','.mpv2','.mpv4','.mov','.mts','.mxf','.ogm','.ogv','.pss','.pva','.qt','.ram','.ratdvd','.rm','.rmm','.rmvb','.roq','.rpm','.smil','.smk','.swf','.tp','.tpr','.ts','.vob','.vp6','.webm','.wm','.wmp','.wmv')
 if (-not ($VIDEO_EXTENSIONS -Contains $File.Extension)) {
-
+    Write-Warning "[$LOG_TAG]Skipping as it does not appear to be a video file"
+    exit 0
 }
 #endregion
 
